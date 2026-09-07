49a405af8a9c16d484c6ea5ef95ad18b2642ef2f
Hanifa Anum <hanifaanum5@gmail.com>
2026-09-07 08:12:01 -0700

    Updated APIs

--- routes/api.php
+++ routes/api.php
@@ -19,25 +19,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/proxy/newtownspares', function (Request $request) {
-    // Get the page number from the query parameters (default to page 1 if not provided)
-    $page = $request->query('page', 1);
+// Route::get('/proxy/newtownspares', function (Request $request) {
+//     // Get the page number from the query parameters (default to page 1 if not provided)
+//     $page = $request->query('page', 1);
 
-    // Construct the URL with the page number
-    $url = 'https://www.trustpilot.com/review/newtownspares.com?page=' . $page;
+//     // Construct the URL with the page number
+//     $url = 'https://www.trustpilot.com/review/newtownspares.com?page=' . $page;
 
-    // Fetch the HTML content from Trustpilot
-    $response = Http::get($url);
+//     // Fetch the HTML content from Trustpilot
+//     $response = Http::get($url);
 
-    // Check if the response is successful
-    if ($response->successful()) {
-        // Return the HTML content of the page
-        return $response->body();
-    }
+//     // Check if the response is successful
+//     if ($response->successful()) {
+//         // Return the HTML content of the page
+//         return $response->body();
+//     }
 
-    // Return an error response if fetching fails
-    return response()->json(['error' => 'Unable to fetch data'], 500);
-});
+//     // Return an error response if fetching fails
+//     return response()->json(['error' => 'Unable to fetch data'], 500);
+// });
 
 // Route::get('/proxy/serverblink', function (Request $request) {
 //     // Get the page number from the query parameters (default to page 1 if not provided)
@@ -89,25 +89,86 @@ Route::get('/proxy/serverblink', function (Request $request) {
     }
 });
 
-Route::get('/proxy/ctspoint', function (Request $request) {
-    // Get the page number from the query parameters (default to page 1 if not provided)
+Route::get('/proxy/newtownspares', function (Request $request) {
     $page = $request->query('page', 1);
 
-    // Construct the URL with the page number
-    $url = 'https://www.trustpilot.com/review/ctspoint.com?page=' . $page;
+    $trustpilotUrl = 'https://www.trustpilot.com/review/newtownspares.com?page=' . $page;
 
-    // Fetch the HTML content from Trustpilot
-    $response = Http::get($url);
+    try {
+        $response = Http::withToken(env('BROWSER_WORKER_KEY'))
+            ->timeout(90)
+            ->post(env('BROWSER_WORKER_URL') . '/render', [
+                'url' => $trustpilotUrl,
+            ]);
 
-    // Check if the response is successful
-    if ($response->successful()) {
-        // Return the HTML content of the page
-        return $response->body();
+        if ($response->successful()) {
+            return $response->json('html');
+        }
+
+        return response()->json([
+            'error' => 'Unable to fetch data',
+            'worker_status' => $response->status(),
+            'worker_response' => $response->body(),
+        ], 500);
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'error' => 'Browser worker connection failed',
+            'message' => $e->getMessage(),
+        ], 500);
     }
-
-    // Return an error response if fetching fails
-    return response()->json(['error' => 'Unable to fetch data'], 500);
 });
+
+
+Route::get('/proxy/ctspoint', function (Request $request) {
+    $page = $request->query('page', 1);
+
+    $trustpilotUrl = 'https://www.trustpilot.com/review/ctspoint.com?page=' . $page;
+
+    try {
+        $response = Http::withToken(env('BROWSER_WORKER_KEY'))
+            ->timeout(90)
+            ->post(env('BROWSER_WORKER_URL') . '/render', [
+                'url' => $trustpilotUrl,
+            ]);
+
+        if ($response->successful()) {
+            return $response->json('html');
+        }
+
+        return response()->json([
+            'error' => 'Unable to fetch data',
+            'worker_status' => $response->status(),
+            'worker_response' => $response->body(),
+        ], 500);
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'error' => 'Browser worker connection failed',
+            'message' => $e->getMessage(),
+        ], 500);
+    }
+});
+
+// Route::get('/proxy/ctspoint', function (Request $request) {
+//     // Get the page number from the query parameters (default to page 1 if not provided)
+//     $page = $request->query('page', 1);
+
+//     // Construct the URL with the page number
+//     $url = 'https://www.trustpilot.com/review/ctspoint.com?page=' . $page;
+
+//     // Fetch the HTML content from Trustpilot
+//     $response = Http::get($url);
+
+//     // Check if the response is successful
+//     if ($response->successful()) {
+//         // Return the HTML content of the page
+//         return $response->body();
+//     }
+
+//     // Return an error response if fetching fails
+//     return response()->json(['error' => 'Unable to fetch data'], 500);
+// });
 
 Route::get('/reviews-nts', [ReviewController::class, 'reviewsNts']);
 Route::get('/stats-nts', [ReviewController::class, 'statsNts']);
