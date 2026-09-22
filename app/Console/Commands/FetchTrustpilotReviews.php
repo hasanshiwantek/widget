920e7dd2c7e584a5776440137458762b32f31181
Hanifa Anum <hanifaanum5@gmail.com>
2026-09-22 06:20:20 -0700

    Updated Reviews Fetch

--- app/Console/Commands/FetchTrustpilotReviews.php
+++ app/Console/Commands/FetchTrustpilotReviews.php
@@ -36,7 +36,7 @@ class FetchTrustpilotReviews extends Command
         $hasMorePages = true;
 
         try {
-            // Review::query()->where('brand', 1)->delete();
+            Review::query()->where('brand', 1)->delete();
 
             while ($hasMorePages) {
                 $url = "https://trust.advertsedge.com/api/proxy/newtownspares?page={$page}";
@@ -117,31 +117,10 @@ class FetchTrustpilotReviews extends Command
                 // }
             }
 
-             /*
-            * Only after ALL pages have been successfully fetched
-            * do we touch the database.
-            */
-            DB::transaction(function () use ($reviews) {
-
-                // Delete old data
-                Review::where('brand', 1)->delete();
-
-                // Insert new data
-                foreach ($reviews as $review) {
-                    Review::create($review);
-                }
-            });
-
             $this->info('Trustpilot reviews have been successfully updated.');
         } catch (\Exception $e) {
             $this->error('Error fetching Trustpilot reviews: ' . $e->getMessage());
-            \Log::error('Error fetching Trustpilot reviews', [
-                'message' => $e->getMessage(),
-            ]);
-
-            return Command::FAILURE;
         }
-        return Command::SUCCESS;
     }
 
     /**
